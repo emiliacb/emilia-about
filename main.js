3263b0d0953e31cc7b8037cd5516bba5a401f8ad
em3c2 <emiliacabralb@gmail.com>
2022-11-07 23:01:53 -0300

    formated code

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-import { render } from "lit-html";
+import { render } from 'lit-html'
 
-import homePage from "./src/pages/index";
-import { LANG } from "./src/utils/constants";
+import homePage from './src/pages/index'
+import { LANG } from './src/utils/constants'
 
 const init = () => {
-  const language =
-    window && window.location.pathname === LANG.esPath ? LANG.es : LANG.en;
-  document.documentElement.setAttribute("lang", language);
-  render(homePage(), document.getElementById("app"));
-};
+  const language = window && window.location.pathname === LANG.esPath ? LANG.es : LANG.en
+  document.documentElement.setAttribute('lang', language)
+  render(homePage(), document.getElementById('app'))
+}
 
-init();
+init()
--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,25 @@
 /* Dependencies */
-import { html, render } from "lit-html";
+import { html, render } from 'lit-html'
 
 /* Utils */
-import { toggleDarkMode } from "../utils";
-import {
-  toggleLanguage,
-  changeLangObserver,
-  getContent,
-} from "../utils/contents";
+import { toggleDarkMode } from '../utils'
+import { toggleLanguage, changeLangObserver, getContent } from '../utils/contents'
 
 class Header extends HTMLElement {
   get template() {
     return html`
       <header class="container relative justify-between items-center">
-        <h1 tabindex="0" class="text-xl font-serif p-3 -ml-3 print:hidden">
-          Emilia <span aria-hidden="true">/</span><span lang="en">About</span>
-        </h1>
-        <h1
-          tabindex="0"
-          class="hidden text-xl font-serif p-3 -ml-3 print:block print:text-3xl"
-        >
-          Emilia Cabral - Fullstack Developer
-        </h1>
+        <h1 tabindex="0" class="text-xl font-serif p-3 -ml-3 print:hidden">Emilia <span aria-hidden="true">/</span><span lang="en">About</span></h1>
+        <h1 tabindex="0" class="hidden text-xl font-serif p-3 -ml-3 print:block print:text-3xl">Emilia Cabral - Fullstack Developer</h1>
         <div class="flex">
           <button
             @click=${toggleLanguage}
             class="print:hidden grid place-content-center w-12 h-12 align-middle focus:rounded-full"
-            aria-label=${getContent("header.languageLabel")}
+            aria-label=${getContent('header.languageLabel')}
             type="button"
           >
-            <span class="text-xs font-serif" aria-hidden="true">
-              ${getContent("header.languageIcon")}
-            </span>
-            <span class="sr-only" aria-live="polite"
-              >${getContent("header.languageFeedback")}</span
-            >
+            <span class="text-xs font-serif" aria-hidden="true"> ${getContent('header.languageIcon')} </span>
+            <span class="sr-only" aria-live="polite">${getContent('header.languageFeedback')}</span>
           </button>
           <button
             @click=${toggleDarkMode}
@@ -46,16 +31,14 @@ class Header extends HTMLElement {
           </button>
         </div>
       </header>
-      <span class="container -mt-12 hidden print:block italic"
-        >emiliacabralb@gmail.com</span
-      >
-    `;
+      <span class="container -mt-12 hidden print:block italic">emiliacabralb@gmail.com</span>
+    `
   }
 
   connectedCallback() {
-    changeLangObserver(() => render(this.template, this));
-    render(this.template, this);
+    changeLangObserver(() => render(this.template, this))
+    render(this.template, this)
   }
 }
 
-customElements.define("header-wc", Header);
+customElements.define('header-wc', Header)
