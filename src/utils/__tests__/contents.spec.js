3b0df1ea8e93bd031ed942503fb7c7ed969a58d7
em3c2 <emiliacabralb@gmail.com>
2022-11-07 22:49:51 -0300

    added contents utils tests

---
+++ src/utils/__tests__/contents.spec.js
@@ -0,0 +1,10 @@
+import { getContent } from '../contents'
+
+console.error = jest.fn()
+
+describe('Contents utils test suite', () => {
+  it('Should return the correct content', () => {
+    expect(getContent('false.path.wording')).toStrictEqual('-')
+    expect(console.error).toBeCalledWith('Missing wording false.path.wording')
+  })
+})
