ecefdf56c212d41483b0d6ca23c19414f7a46b3a
em3c2 <emiliacabralb@gmail.com>
2022-11-08 16:07:34 -0300

    changed style and added motion reduce config removed footer tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,19 +15,17 @@ const homePage = () => html`
   <main class="container flex-col md:flex-row" id="main">
     <nav-wc></nav-wc>
     <div class="md:ml-56 mt-12 print:-mt-16">${SECTIONS.map((name) => html`<section-wc name=${name}></section-wc>`)}</div>
-    <footer class="relative h-24">
-      <a
-        href="https://github.com/Em3c2/emilia-about"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="fixed w-12 h-12 flex lg:w-10 lg:h-10 pl-4 pt-4 lg:p-2 right-0 bottom-0 lg:right-10 lg:bottom-10 bg-stone-300 text-black dark:text-black rounded-tl-full lg:rounded-full overflow-hidden"
-      >
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
-        </svg>
-        <span class="sr-only">Github</span>
-      </a>
-    </footer>
+    <a
+      href="https://github.com/Em3c2/emilia-about"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="fixed w-12 h-12 flex lg:w-10 lg:h-10 pl-4 pt-4 lg:p-2 right-0 bottom-0 lg:right-10 lg:bottom-10 backdrop-blur text-black lg:bg-stone-300 lg:hover:animate-spin rounded-tl-full lg:rounded-full overflow-hidden"
+    >
+      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+      </svg>
+      <span class="sr-only">Github</span>
+    </a>
   </main>
   <div aria-hidden="true" class="theme-transition"></div>
 `
