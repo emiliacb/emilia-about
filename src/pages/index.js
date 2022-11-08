e702f2f336dc63c42cf242b93951e4d85599fad9
em3c2 <emiliacabralb@gmail.com>
2022-11-08 16:10:12 -0300

    fix color in darkmode

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const homePage = () => html`
       href="https://github.com/Em3c2/emilia-about"
       target="_blank"
       rel="noopener noreferrer"
-      class="fixed w-12 h-12 flex lg:w-10 lg:h-10 pl-4 pt-4 lg:p-2 right-0 bottom-0 lg:right-10 lg:bottom-10 backdrop-blur text-black lg:bg-stone-300 lg:hover:animate-spin rounded-tl-full lg:rounded-full overflow-hidden"
+      class="fixed w-12 h-12 flex lg:w-10 lg:h-10 pl-4 pt-4 lg:p-2 right-0 bottom-0 lg:right-10 lg:bottom-10 backdrop-blur lg:text-black lg:bg-stone-300 lg:hover:animate-spin rounded-tl-full lg:rounded-full overflow-hidden"
     >
       <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
