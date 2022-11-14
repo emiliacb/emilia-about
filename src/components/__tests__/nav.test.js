23cba6461a46298b461bd69a090392241ae23653
em3c2 <emiliacabralb@gmail.com>
2022-11-14 10:56:03 -0300

    added window to setTimeout

--- src/components/__tests__/nav.test.js
+++ src/components/__tests__/nav.test.js
@@ -12,7 +12,7 @@ describe('Nav test suite', () => {
     expect(section).toBeDefined()
   })
   it('Should render sections links', async () => {
-    setTimeout = jest.fn((cb) => cb())
+    window.setTimeout = jest.fn((cb) => cb())
     const component = await render()
     const ariaLiveFeedback = component.querySelector('#aria-live-feedback')
     fireEvent.click(queryByText(component, 'Deactivate animations'))
