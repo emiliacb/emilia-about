5cefbc87f565492817a2df655070eafe8d837676
em3c2 <emiliacabralb@gmail.com>
2022-11-14 13:48:35 -0300

    changed url

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ import { LANG, EVENTS, TRACKS_TYPES } from './src/utils/constants'
 const language = window && window.location.pathname === LANG.esPath ? LANG.es : LANG.en
 document.documentElement.setAttribute('lang', language)
 
-const isDevelopment = `https://${import.meta.env.VERCEL_ENV}` === 'development' || import.meta.env.MODE === 'development'
-const APP_URL = isDevelopment ? import.meta.env.VITE_APP_URL : import.meta.env.VITE_VERCEL_URL
+const isDevelopment = import.meta.env.VERCEL_ENV === 'development' || import.meta.env.MODE === 'development'
+const APP_URL = isDevelopment ? import.meta.env.VITE_APP_URL : `https://${import.meta.env.VITE_VERCEL_URL}`
 
 /* Tracking view */
 fetch(`${APP_URL}/api/metrics?event=${EVENTS.homepage.view}&type=${TRACKS_TYPES.view}`).catch((error) => {
