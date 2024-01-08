f37b12411fe87801f89118a899864948064e9e2a
em3c2 <emiliacabralb@gmail.com>
2024-01-07 21:27:47 -0300

    added pdf link

--- src/components/header.js
+++ src/components/header.js
@@ -15,10 +15,7 @@ class Header extends HTMLElement {
   get template() {
     return html`
       <header class="container relative justify-between items-center">
-        <a
-          class="visible-on-focus bg-black dark:bg-white text-white dark:text-black focus-visible:p-5 focus-visible:absolute"
-          href="#experience"
-        >
+        <a class="visible-on-focus bg-black dark:bg-white text-white dark:text-black focus-visible:p-5 focus-visible:absolute" href="#experience">
           ${getContent('header.skip')}
         </a>
         <button
@@ -64,20 +61,20 @@ class Header extends HTMLElement {
               />
             </svg>
           </button>
-          <button
-            @click=${() => window && window.print()}
-            class="hidden md:grid print:!hidden place-content-center w-12 h-12 align-middle focus:rounded-full transition-transform duration-300 hover:scale-125"
+          <a
+            class="grid print:!hidden place-content-center text-3xl w-12 h-12 align-middle focus:rounded-full transition-transform duration-300 hover:scale-125"
             aria-label=${getContent('header.printLabel')}
-            type="button"
+            href="/Emilia_Cabral_CV.pdf"
+            download
           >
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
               <path
-                fillRule="evenodd"
-                d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                clipRule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
               />
             </svg>
-          </button>
+          </a>
         </div>
       </header>
       <span class="container -mt-12 hidden print:block italic">emiliaborg.ar</span>
